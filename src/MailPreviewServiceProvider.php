694d1e4b828ec37c768d4ff1b28aeb5afc9d16a4
Steve Lacey <steve@stevelacey.net>
2018-09-28 18:01:39 +0200

    Always register mailpreview routes

--- src/MailPreviewServiceProvider.php
+++ src/MailPreviewServiceProvider.php
@@ -16,20 +16,20 @@ class MailPreviewServiceProvider extends ServiceProvider
     {
         $this->publishes([__DIR__ . '/config/mailpreview.php' => config_path('mailpreview.php')], 'config');
 
+        $this->app['router']->group(['middleware' => $this->middleware()], function ($router) {
+            $router->get('/mailpreview')->uses(MailPreviewController::class . '@index')->name('mailpreview');
+            $router->get('/mailpreview/{path}.eml')->uses(MailPreviewController::class . '@download')->name('mailpreview_eml');
+            $router->get('/mailpreview/{path}.html')->uses(MailPreviewController::class . '@html')->name('mailpreview_html');
+            $router->get('/mailpreview/view-source/{path}.html')->uses(MailPreviewController::class . '@source')->name('mailpreview_source');
+            $router->get('/mailpreview/{path}.txt')->uses(MailPreviewController::class . '@text')->name('mailpreview_text');
+            $router->get('/mailpreview/{path}')->uses(MailPreviewController::class . '@show')->name('mailpreview.show');
+        });
+
         if ($this->app['config']['mail.driver'] != 'preview') {
             return;
         }
 
         if ($this->app['config']['mailpreview.show_link_to_preview']) {
-            $this->app['router']->group(['middleware' => $this->middleware()], function ($router) {
-                $router->get('/mailpreview')->uses(MailPreviewController::class . '@index')->name('mailpreview');
-                $router->get('/mailpreview/{path}.eml')->uses(MailPreviewController::class . '@download')->name('mailpreview_eml');
-                $router->get('/mailpreview/{path}.html')->uses(MailPreviewController::class . '@html')->name('mailpreview_html');
-                $router->get('/mailpreview/view-source/{path}.html')->uses(MailPreviewController::class . '@source')->name('mailpreview_source');
-                $router->get('/mailpreview/{path}.txt')->uses(MailPreviewController::class . '@text')->name('mailpreview_text');
-                $router->get('/mailpreview/{path}')->uses(MailPreviewController::class . '@show')->name('mailpreview.show');
-            });
-
             $this->app[Kernel::class]->pushMiddleware(MailPreviewMiddleware::class);
 
             $this->publishes([__DIR__ . '/resources/views/notification.blade.php' => resource_path('views/vendor/mailpreview/notification.blade.php')], 'views');
